74b55ed8a57e7b5efd2e46a6ce27fbaa75c07174
Delbrique <valentinbiyong2@gmail.com>
2025-11-09 15:46:32 +0100

    test: code conforme après test du hook

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ const PORT = 3004;
 app.use(express.json());
 app.use("/projects", projectsRouter);
 // app.js ou index.js
-app.get('/', (req, res) => {
-  res.json({ message: 'API REST TP opérationnelle' });
-})
+app.get("/", (req, res) => {
+  res.json({ message: "API REST TP opérationnelle" });
+});
 
 app.listen(PORT, () => {
-  console.log(`? Serveur lancé sur http://localhost:${PORT}`)
+  console.log(`? Serveur lancé sur http://localhost:${PORT}`);
 });
