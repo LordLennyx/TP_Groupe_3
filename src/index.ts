2a6a53941cb3709e75a2b04a44aef195e79b136a
Delbrique <valentinbiyong2@gmail.com>
2025-11-09 15:39:29 +0100

    chore: avancer sur le tp malgré config eslint

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ app.use("/projects", projectsRouter);
 // app.js ou index.js
 app.get('/', (req, res) => {
   res.json({ message: 'API REST TP opérationnelle' });
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`? Serveur lancé sur http://localhost:${PORT}`);
+  console.log(`? Serveur lancé sur http://localhost:${PORT}`)
 });
