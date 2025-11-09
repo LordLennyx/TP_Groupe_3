d836b14bbf0ed5ec532affd6de970f0eca90c8ae
Delbrique <valentinbiyong2@gmail.com>
2025-11-09 16:55:34 +0100

    feat: add new project endpoint (fixes #1)

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -10,11 +10,11 @@ import {
 
 const router = express.Router();
 
-router.get("/", getAllProjects); 
-router.post("/", createProject); 
-router.get("/:id", getProjectById); 
-router.put("/:id/grade", updateProjectGrade); 
-router.delete("/:id", deleteProject); 
-router.get("/course/:courseName", getProjectsByCourse); 
+router.get("/", getAllProjects);
+router.post("/", createProject);
+router.get("/:id", getProjectById);
+router.put("/:id/grade", updateProjectGrade);
+router.delete("/:id", deleteProject);
+router.get("/course/:courseName", getProjectsByCourse);
 
 export default router;
